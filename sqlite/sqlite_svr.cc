1567b0927de4f0067b46868753a675a45bf9de89
Xuhao Luo <rossihox@gmail.com>
2023-04-19 21:48:58 -0500

    set locking mode to exclusive avoid using shared memory lock as it performs bad on cephfs

--- sqlite/sqlite_svr.cc
+++ sqlite/sqlite_svr.cc
@@ -364,7 +364,8 @@ void server_func(erpc::Nexus *nexus, int thread_id, const utils::Properties *pro
         throw utils::Exception(std::string("SQLite Open: ") + sqlite3_errmsg(c.db_));
       }
 
-      ret = sqlite3_exec(c.db_, "PRAGMA journal_mode=WAL; PRAGMA synchronous=NORMAL;", nullptr, nullptr, &err_msg);
+      ret = sqlite3_exec(c.db_, "PRAGMA locking_mode=EXCLUSIVE; PRAGMA journal_mode=WAL; PRAGMA synchronous=NORMAL;",
+                         nullptr, nullptr, &err_msg);
       if (ret != SQLITE_OK) {
         sqlite3_close(c.db_);
         throw utils::Exception(std::string("Can't set journal mode to WAL, error: ") + err_msg);
